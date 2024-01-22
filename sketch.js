649738145afab1addb565a2df7a2f62c46d47fec
DianaVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-01-22 15:08:45 +0000

    some random shapes

--- sketch.js
+++ sketch.js
@@ -7,4 +7,22 @@ function setup() {
 
 function draw() {
   background(220);
+
+  const colorOne = color(252, 186, 3);
+
+  fill(colorOne);
+
+  rect(500, 23, 200, 100);
+
+  const colorTwo = color(252, 90, 3);
+
+  fill(colorTwo);
+
+  circle(100, 100, 50);
+
+  const colorThree = color(10, 200, 30);
+
+  fill(colorThree);
+
+  ellipse(400, 700, 50, 120);
 }
